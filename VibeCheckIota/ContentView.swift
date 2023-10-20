fb9e970f93bf06975a18a82898e607837e9f8cf7
dweslynch <dweslynch@gmail.com>
2023-10-20 00:07:05 -0400

    Initial Commit

---
+++ VibeCheckIota/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  VibeCheckIota
+//
+//  Created by Devin Lynch on 10/20/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ VibeCheckIota/VibeCheckIotaApp.swift
@@ -0,0 +1,17 @@
+//
+//  VibeCheckIotaApp.swift
+//  VibeCheckIota
+//
+//  Created by Devin Lynch on 10/20/23.
+//
+
+import SwiftUI
+
+@main
+struct VibeCheckIotaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
